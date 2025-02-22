9d22739f24b519335c8aa298b0150c1e649ddcd4
Olivier <kibakodev@gmail.com>
2025-02-22 17:58:41 -0500

    Update Source

---
+++ Source/DOOM2025/DOOM2025.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DOOM2025.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DOOM2025, "DOOM2025" );
